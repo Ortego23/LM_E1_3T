5446b01560bf912a8d4e38a2296ff84773ad7f28
Ortego23 <156656663+Ortego23@users.noreply.github.com>
2024-05-27 11:15:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,270 @@
+const data = [
+    {
+        "CIF": "B0231535372",
+        "NOMBRE": "HERNANDEZ Y ATENCIA SL",
+        "CUPS": "ES002100000358263FN",
+        "TARIFA": "6.1TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "FRAN VILLAROBLEDO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "SE LE HA HECHO CAMBIO DE POTENCIA",
+        "FECHA": "15-Nov"
+    },
+    {
+        "CIF": "B16993131",
+        "NOMBRE": "TE COMPRO Y PUNTO SL",
+        "CUPS": "ES0021000004888369LS",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "FIRMADO",
+        "FECHA": "21-Nov",
+        "PAGADO": 60,
+        "O 50%": 30
+    },
+    {
+        "CIF": "52760917K",
+        "NOMBRE": "MARIA DOLORES DOMINGUEZ",
+        "CUPS": "ES0021000000394104RL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "FRAN VILLAROBLEDO",
+        "ESTADO": "TRAMITADO",
+        "FECHA": "15-Nov",
+        "PAGADO": 35
+    },
+    {
+        "CIF": "52760917K",
+        "NOMBRE": "MARIA DOLORES DOMINGUEZ",
+        "CUPS": "ES0220040039750113KK",
+        "TARIFA": "RL1",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "FRAN VILLAROBLEDO",
+        "ESTADO": "TRAMITADO",
+        "FECHA": "15-Nov",
+        "PAGADO": 25
+    },
+    {
+        "CIF": "05247710F",
+        "NOMBRE": "ROCIO GONZALEZ DOS SANTOS",
+        "CUPS": "ES0315000011413712MD",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "CHUS",
+        "ESTADO": "TRAMITADO",
+        "FECHA": "15-Nov",
+        "PAGADO": 35
+    },
+    {
+        "CIF": "04854540E",
+        "NOMBRE": "RAUL BARDERA MARTIN",
+        "CUPS": "ES0021000001813732SH",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO"
+    },
+    {
+        "CIF": "B04979373",
+        "NOMBRE": "THINK ZERO SL",
+        "CUPS": "ES0021000019532063QT",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B88443080",
+        "NOMBRE": "BE BRAVE SL",
+        "CUPS": "ES0031104407910004DN",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B78856630",
+        "NOMBRE": "TALLERES PASAMON",
+        "CUPS": "ES0021000010584909PF",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "ROBERTO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "",
+        "FECHA": "22/2/2023",
+        "PAGADO": "",
+        "O 50%": "",
+        "DOCUMENTOS ADJUNTOS": "CARPETA CONTRATOS  DE BASE DE DATOS\\talleres pasamon",
+        "FECHA DE ACABAR CONTRATO": ""
+    },
+    {
+        "CIF": "B78856630",
+    "NOMBRE": "TALLERES PASAMON",
+    "CUPS": "ES0021000005598389RW",
+    "TARIFA": "30TD",
+    "COMERCIALIZADORA": "EVOLVE",
+    "COMERCIAL": "ROBERTO",
+    "ESTADO": "TRAMITADO",
+    "ACCIONES": "",
+    "FECHA": "22/2/2023",
+    "PAGADO": "",
+    "O 50%": "",
+    "DOCUMENTOS ADJUNTOS": "CARPETA CONTRATOS  DE BASE DE DATOS\\talleres pasamon\\contrato 1.pdf",
+    "FECHA DE ACABAR CONTRATO": ""
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "06584149P",
+        "NOMBRE": "RUBEN MARTIN CUEVA",
+        "CUPS": "ES0315000011605448SV",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "CHUS",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "",
+        "FECHA": "27/2/2023",
+        "PAGADO": "",
+        "O 50%": "",
+        "DOCUMENTOS ADJUNTOS": "CARPETA CONTRATOS  DE BASE DE DATOS\\ruben martin cuerva monica",
+        "FECHA DE ACABAR CONTRATO": ""
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "Y0549553M",
+        "NOMBRE": "EMANUEL DANIEL BELEA",
+        "CUPS": "ES0022000008700394GX",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "ALUZ",
+        "COMERCIAL": "ROBERTO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "CAMBIO DE TITULAR YA ESTABA CON ALUZ",
+        "FECHA": "16/2/2023",
+        "PAGADO": "",
+        "O 50%": "",
+        "DOCUMENTOS ADJUNTOS": "CARPETA CONTRATOS  DE BASE DE DATOS\\EMANUEL DANIEL BELEA.pdf",
+        "FECHA DE ACABAR CONTRATO": ""
+    },
+    {
+        "CIF": "B87622195",
+        "NOMBRE": "CAVEREL ASSETS SL",
+        "CUPS": "ES0021000011732140TK",
+        "TARIFA": "30TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "PAGADO": 525.87,
+        "O 50%": 262.93
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "CUPS": "ES0021000022413423BN",
+        "TARIFA": "30TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "PAGADO": 654.31,
+        "O 50%": 327.15
+    },
+    {
+        "CIF": "70815146D",
+        "NOMBRE": "CESAR RIVAS JIMENEZ",
+        "CUPS": "ES0315000020705751NW",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "CHUS",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "",
+        "FECHA": "26/1/2023",
+        "PAGADO": "",
+        "O 50%": "",
+        "DOCUMENTOS ADJUNTOS": "CARPETA CONTRATOS  DE BASE DE DATOS\\CESAR RIVAS",
+        "FECHA DE ACABAR CONTRATO": ""
+    },
+    {
+        "CIF": "B80149986",
+        "NOMBRE": "CASTELLANA GOLF ",
+        "CUPS": "ES0021000020399272TV",
+        "TARIFA": "30TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "PAGADO": 403.35,
+        "O 50%": 201.68
+    }
+];
+
+
+const container = document.getElementById('data-container');
+
+function renderData(data) {
+    container.innerHTML = '';
+    data.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.classList.add('data-item');
+        itemElement.innerHTML = `
+            <p><strong>CIF:</strong> ${item.CIF}</p>
+            <p><strong>Nombre:</strong> ${item.NOMBRE}</p>
+            <p><strong>CUPS:</strong> ${item.CUPS}</p>
+            <p><strong>Tarifa:</strong> ${item.TARIFA}</p>
+            <p><strong>Comercializadora:</strong> ${item.COMERCIALIZADORA}</p>
+            <p><strong>Comercial:</strong> ${item.COMERCIAL}</p>
+            <p><strong>Estado:</strong> ${item.ESTADO}</p>
+            <p><strong>Acciones:</strong> ${item.ACCIONES}</p>
+            <p><strong>Fecha:</strong> ${item.FECHA}</p>
+            <p><strong>Pagado:</strong> ${item.PAGADO}</p>
+            <p><strong>50%:</strong> ${item["O 50%"]}</p>
+        `;
+        container.appendChild(itemElement);
+    });
+}
+
+function searchData() {
+    const cif = document.getElementById('search-cif').value.toLowerCase();
+    const nombre = document.getElementById('search-nombre').value.toLowerCase();
+    const comercializadora = document.getElementById('search-comercializadora').value.toLowerCase();
+    const estado = document.getElementById('search-estado').value.toLowerCase();
+
+    const filteredData = data.filter(item => {
+        return (
+            (!cif || item.CIF.toLowerCase().includes(cif)) &&
+            (!nombre || item.NOMBRE.toLowerCase().includes(nombre)) &&
+            (!comercializadora || item.COMERCIALIZADORA.toLowerCase().includes(comercializadora)) &&
+            (!estado || item.ESTADO.toLowerCase().includes(estado))
+        );
+    });
+
+    renderData(filteredData);
+}
+
+renderData(data);
+
+
